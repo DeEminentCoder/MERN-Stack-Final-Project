34a4b1c3e52742e53c41afff7b628273b23f77cc
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-11-19 15:16:04 +0100

    2DAYS

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path='/' element></Route>
+      <Route path='' element></Route>
     </Routes>
     </BrowserRouter>
   )
