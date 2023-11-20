388ab9844908adecb52ad31a8f6a3ed66839d87c
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2023-11-20 16:51:44 +0100

    created routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/about";
+import SignUp from "./pages/SignUp";
+import Signin from "./pages/Signin";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='' element></Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<About />} />
+        <Route path="/" element={<Signin />} />
+        <Route path="/" element={<SignUp />} />
+        <Route path="/" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
