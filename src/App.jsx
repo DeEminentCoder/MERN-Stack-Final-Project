8c5d58fdfce452ac9f88ed865f13331ce029c45c
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2024-02-02 15:38:23 +0100

    user/profile interface modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/about";
+import About from "./pages/About";
 import SignUp from "./pages/SignUp";
 import Signin from "./pages/Signin";
 import Profile from "./pages/Profile";
-import Header from "./components/header";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/profile" element={<Profile />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
