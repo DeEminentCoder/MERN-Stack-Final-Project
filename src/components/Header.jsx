698ebb6fd36d125889be7987ef579ce6dc5984f6
Kehinde Olaleye Simeon <simeonkenny66@gmail.com>
2024-02-05 18:51:20 +0100

    create listing and show listing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,28 @@
 // import React from "react";
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import { useSelector } from 'react-redux';
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const Header = () => {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-white shadow-md">
       <div className=" flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -13,6 +31,7 @@ const Header = () => {
         </Link>
 
         <form
+          onSubmit={handleSubmit}
           className="bg-orange-50 p-3 rounded-lg flex items-center"
           action=""
         >
@@ -20,6 +39,8 @@ const Header = () => {
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-24 sm:w-64 text-gray-600"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
           <FaSearch className="text-orange-500" />
         </form>
@@ -35,15 +56,15 @@ const Header = () => {
               About
             </li>
           </Link>
-          <Link to='/profile'>
+          <Link to="/profile">
             {currentUser ? (
               <img
-                className='rounded-full h-7 w-7 object-cover'
+                className="rounded-full h-7 w-7 object-cover"
                 src={currentUser.avatar}
-                alt='profile'
+                alt="profile"
               />
             ) : (
-              <li className=' text-slate-700 hover:underline'> Sign in</li>
+              <li className=" text-slate-700 hover:underline"> Sign in</li>
             )}
           </Link>
         </ul>
